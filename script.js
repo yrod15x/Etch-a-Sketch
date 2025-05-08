fed96e9006f094cb92eb55be18c345c8027c046f
yrod15x <megasoldier15x@hotmail.com>
2025-05-08 12:47:24 -0400

    Page Updated

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const main = document.querySelector(".contenedor");
+const boton = document.querySelector(".boton");
+var tamGrilla = 16;
+var grilla = document.querySelector(".contenedor");
 
-for(let i = 0; i < 16; i++)
+function crearGrilla(tamGrilla)
 {
-    for(let j = 0; j < 16; j++)
-    {
-        const div = document.createElement("div");
-        div.classList.add("bloques");
-        main.appendChild(div);
-    } 
+    grilla.style.width = `${80}%`;
+    console.log(grilla.style.width);
+    for(let i = 0; i < tamGrilla; i++)
+        {
+            for(let j = 0; j < tamGrilla; j++)
+            {
+                var div = document.createElement("div");
+                div.classList.add("bloques");
+                div.style.width = `${100 / tamGrilla}%`;
+                div.style.height = `${(100 / tamGrilla)}dvh`;
+                grilla.appendChild(div);
+            } 
+        }
 }
+
+crearGrilla(tamGrilla);
+
+boton.addEventListener("click", function()
+{   //Borrar los elementos del contenedor y crear una nueva grilla
+    tamGrilla = Number(prompt("Enter the grid size"));
+    if(tamGrilla < 2)
+    {
+        tamGrilla = 2;
+    }
+    grilla.innerHTML = "";
+    crearGrilla(tamGrilla);
+
+});
+
